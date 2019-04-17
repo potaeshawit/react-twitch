7a89cde27d86fdbe84d93783cd919f935faac010
Taeshawit Suwanwigo <s.taeshawit@gmail.com>
2019-04-17 15:11:50 +0700

    navigating with react router

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
 const PageOne = () => {
-  return <div>PageOne</div>;
+  return (
+    <div>
+      PageOne
+      <Link to="/pagetwo">Navigate to PageTwo</Link>
+    </div>
+  );
 };
 
 const PageTwo = () => {
